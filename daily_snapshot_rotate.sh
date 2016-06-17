28baf1208add99ef404d7ab175bb4e4be372f694
Claas-Fridtjof Lisowski <claas@mbp15_wifi.lis.priv>
2016-06-17 23:47:12 +0200

    change daily backup rotate to variable paths

--- daily_snapshot_rotate.sh
+++ daily_snapshot_rotate.sh
@@ -18,48 +18,53 @@ CP=/bin/cp;
 MYID="$$"
 
 # ------------- file locations -----------------------------------------
-
 MOUNT_DEVICE=NFS_SERVER_IP:/NFS_SHARE;
 MOUNT_POINT=/root/backup;
-# EXCLUDES=/var/scripts/excludes.txt; for files
-EXCLUDE1=$3;
-EXCLUDE2=$4;
-SRC_FOLDER=$1;
-DEST_FOLDER=$2;
 INPROGRESS_FILE="$MOUNT_POINT/backup.inprogress";
+ROTATE_FOLDER=$1;
 
 # ------------- the script itself --------------------------------------
+MOUNT_RO_STATUS=$(fgrep -c 'root/snapshot nfs ro,' /proc/mounts)
 
 # make sure we're running as root
 if (( `$ID -u` != 0 )); then { $ECHO "Sorry, must be root.  Exiting..."; exit; } fi
 
-# attempt to remount the RW mount point as RW; else abort
-$MOUNT -o remount,rw $MOUNT_DEVICE $SNAPSHOT_RW ;
-if (( $? )); then
-{
-        $ECHO "snapshot: could not remount $SNAPSHOT_RW readwrite";
-        exit;
-}
-fi;
+if [ "$MOUNT_RO_STATUS" -eq "1" ];
+        then
+        echo "Mounted read only - remount rw"
+        # attempt to remount the RW mount point as RW; else abort
+        $MOUNT -o remount,rw $MOUNT_DEVICE $MOUNT_POINT ;
+        if (( $? )); then
+        {
+                $ECHO "snapshot: could not remount $MOUNT_POINT readwrite";
+                exit;
+        }
+        fi;
+[[ -f $INPROGRESS_FILE ]] && rm -f $INPROGRESS_FILE
+fi
+
+echo $MYID >> $INPROGRESS_FILE
+chmod 600 $INPROGRESS_FILE
+echo $MYID
 
 
 # step 1: delete the oldest snapshot, if it exists:
-if [ -d $SNAPSHOT_RW/home/daily.2 ] ; then                      \
-$RM -rf $SNAPSHOT_RW/home/daily.2 ;                             \
+if [ -d $ROTATE_FOLDER/daily.2 ] ; then                      \
+$RM -rf $ROTATE_FOLDER/daily.2 ;                             \
 fi ;
 
 # step 2: shift the middle snapshots(s) back by one, if they exist
-if [ -d $SNAPSHOT_RW/home/daily.1 ] ; then                      \
-$MV $SNAPSHOT_RW/home/daily.1 $SNAPSHOT_RW/home/daily.2 ;       \
+if [ -d $ROTATE_FOLDER/daily.1 ] ; then                      \
+$MV $ROTATE_FOLDER/daily.1 $ROTATE_FOLDER/daily.2 ;       \
 fi;
-if [ -d $SNAPSHOT_RW/home/daily.0 ] ; then                      \
-$MV $SNAPSHOT_RW/home/daily.0 $SNAPSHOT_RW/home/daily.1;        \
+if [ -d $ROTATE_FOLDER/daily.0 ] ; then                      \
+$MV $ROTATE_FOLDER/daily.0 $ROTATE_FOLDER/daily.1;        \
 fi;
 
 # step 3: make a hard-link-only (except for dirs) copy of
 # hourly.3, assuming that exists, into daily.0
-if [ -d $SNAPSHOT_RW/home/hourly.3 ] ; then                     \
-$CP -al $SNAPSHOT_RW/home/hourly.3 $SNAPSHOT_RW/home/daily.0 ;  \
+if [ -d $ROTATE_FOLDER/hourly.3 ] ; then                     \
+$CP -al $ROTATE_FOLDER/hourly.3 $ROTATE_FOLDER/daily.0 ;  \
 fi;
 
 # note: do *not* update the mtime of daily.0; it will reflect
