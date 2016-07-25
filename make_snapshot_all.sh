6c364492ca394c89fd6bb1c66c0d817f7eafffee
Blacs30 <github@lisowski-development.de>
2016-07-26 00:10:43 +0200

    temporaray status of that file

--- make_snapshot_all.sh
+++ make_snapshot_all.sh
@@ -11,6 +11,23 @@
 # ----------------------------------------------------------------------
 
 # ------------- system commands used by this script --------------------
+exec &> /tmp/backuplog.txt
+
+### Defaults Setup ###
+STORAGEDIR="/root/snapshot/backup/db";
+NOW=`date "+%s"`;
+OLDESTDIR=`ls $STORAGEDIR | head -1`;
+NOWDIR=`date +"%Y-%m-%d"`;
+NOWFILE=`date +"%Y-%m-%d"`;
+OLDEST=`date -d "$OLDESTDIR" "+%s"`;
+BACKUPDIR="$STORAGEDIR/$NOWDIR";
+DIFF=$(($NOW-$OLDEST));
+DAYS=$(($DIFF/ (60*60*24)));
+DIRLIST=`ls -lRh $BACKUPDIR`;
+ROTATION="7"
+GZIPCHECK=();
+
+
 ID=/usr/bin/id;
 ECHO=/bin/echo;
 MOUNT=/bin/mount;
@@ -25,11 +42,6 @@ MYID="$$"
 
 MOUNT_DEVICE=NFS_SERVER_IP:/NFS_SHARE;
 MOUNT_POINT=/root/backup;
-# EXCLUDES=/var/scripts/excludes.txt; for files
-EXCLUDE1=$3;
-EXCLUDE2=$4;
-SRC_FOLDER=$1;
-DEST_FOLDER=$2;
 INPROGRESS_FILE="$MOUNT_POINT/backup.inprogress";
 
 # ------------- the script itself --------------------------------------
@@ -93,8 +105,6 @@ if [[ -n $1 ]]; then
     tail -1 $1
 fi
 
-
-
 for i in "${EXCLUDES[@]}"
 do
 RSYNC_EXCLUDE="$RSYNC_EXCLUDE --exclude $i"
@@ -102,27 +112,6 @@ done
 echo $RSYNC_EXCLUDE
 
 
-
-
-
-# step 1: delete the oldest snapshot, if it exists:
-if [ -d $DEST_FOLDER/hourly.3 ] ; then                     \
-$RM -rf $DEST_FOLDER/hourly.3 ;                            \
-fi ;
-
-# step 2: shift the middle snapshots(s) back by one, if they exist
-if [ -d $DEST_FOLDER/hourly.2 ] ; then                     \
-$MV $DEST_FOLDER/hourly.2 $DEST_FOLDER/hourly.3 ;     \
-fi;
-if [ -d $DEST_FOLDER/hourly.1 ] ; then                     \
-$MV $DEST_FOLDER/hourly.1 $DEST_FOLDER/hourly.2 ;     \
-fi;
-
-# step 3: make a hard-link-only (except for dirs) copy of the latest snapshot,
-# if that exists
-if [ -d $DEST_FOLDER/hourly.0 ] ; then                     \
-$CP -al $DEST_FOLDER/hourly.0 $DEST_FOLDER/hourly.1 ; \
-fi;
 
 # step 4: rsync from the system into the latest snapshot (notice that
 # rsync behaves like cp --remove-destination by default, so the destination
@@ -133,19 +122,16 @@ $RSYNC                                                          \
         --exclude="$EXCLUDE1" --exclude="$EXCLUDE2"             \
         $SRC_FOLDER $DEST_FOLDER/hourly.0 ;
 
-# step 5: update the mtime of hourly.0 to reflect the snapshot time
-$TOUCH $DEST_FOLDER/hourly.0 ;
+
+
 
 # remove entry in progress file
 sed -i "/.*$MYID.*/d" $INPROGRESS_FILE
 
 # count lines in file
 ACTIVE_SYNCS=$(wc -l $INPROGRESS_FILE | awk '{print $1}')
-# count active processes in scripts dirctory
-ACTIVE_PROCESSES=$(ps aux | grep -c '\/var\/scripts\/[a-zA-Z0-9]*.sh')
 
-
-if [[ $ACTIVE_SYNCS -ge 1 ]] || [[ $ACTIVE_PROCESSES -ge 1 ]]
+if [[ $ACTIVE_SYNCS -ge 1 ]]
         then
         echo "Other syncs are still running - don't remount ro"
 else
